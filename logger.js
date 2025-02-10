5c6fde5bf6262988897684f75e6b08665d08d3ee
Sherry <sm718036@gmail.com>
2025-02-10 15:14:46 +0500

    initial commit

---
+++ logger.js
@@ -0,0 +1,19 @@
+/*jshint esversion: 8 */
+const pino = require("pino");
+
+let logger;
+
+if (process.env.NODE_ENV !== "production") {
+  // In non-production environments, log to the console
+  logger = pino({
+    level: "debug",
+    transport: {
+      target: "pino-pretty",
+    },
+  });
+} else {
+  // production
+  logger = pino();
+}
+
+module.exports = logger;
